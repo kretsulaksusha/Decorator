65a0da31c0747a54cd3ec5a3504d03441d4cd5fa
kretsulaksusha <kretsula.pn@ucu.edu.ua>
2023-12-23 21:39:22 +0200

    renamed file

--- src/main/java/ua/edu/ucu/apps/lab10/Lab10Application.java
+++ src/main/java/ua/edu/ucu/apps/lab10/Lab10Application.java
@@ -5,10 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-public class Lab8Application {
+public class Lab10Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Lab8Application.class, args);
+		SpringApplication.run(Lab10Application.class, args);
 	}
 
 }
